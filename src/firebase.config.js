c94a37536a95b372ea4562b711b9be56eee7e82e
Dushyant Khandelwal <dushyantkhandelwal4665@gmail.com>
2024-08-28 12:36:29 +0530

    add

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,13 +4,20 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyChrWfboE_TSqN5JXVupVq5knNl8AYFFRk",
-  authDomain: "flex-burn-auth.firebaseapp.com",
-  projectId: "flex-burn-auth",
-  storageBucket: "flex-burn-auth.appspot.com",
-  messagingSenderId: "617970663565",
-  appId: "1:617970663565:web:e9b31f7f7e659f120f62c4",
-  measurementId: "G-0G0T9ZTD0H"
+  // apiKey: "AIzaSyChrWfboE_TSqN5JXVupVq5knNl8AYFFRk",
+  // authDomain: "flex-burn-auth.firebaseapp.com",
+  // projectId: "flex-burn-auth",
+  // storageBucket: "flex-burn-auth.appspot.com",
+  // messagingSenderId: "617970663565",
+  // appId: "1:617970663565:web:e9b31f7f7e659f120f62c4",
+  // measurementId: "G-0G0T9ZTD0H"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
